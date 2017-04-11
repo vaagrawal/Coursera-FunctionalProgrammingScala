5ae9f908917a83bbab1d37000f5732821abdf7d3
Vaibhav <vaibhavajay@gmail.com>
2017-04-11 11:15:37 -0700

    Recursion Tutorial

--- src/main/scala-2.12/ScalaLearning/Week1/Recursion.scala
+++ src/main/scala-2.12/ScalaLearning/Week1/Recursion.scala
@@ -1,5 +1,44 @@
+
 object Recursion extends App {
-  val a =1
-  a + 1
+
+  def squareRoot(x: Double) : Double = {
+
+    def squareRootItr(guess: Double): Double = {
+      if (isBetterGuess(guess)) guess
+      else squareRootItr(improveGuess(guess))
+    }
+
+    def isGoodGuess(guess: Double): Boolean = {
+      Math.abs(guess * guess - x) < 0.001
+    }
+
+    // This is better beacuse this solves the issue with small and very large numbers.
+    // Previously, the abs function checks the difference and compares to epsilon where the issue was that the consecutive absolute
+    // values had passed the epsilon and thus it was an unending loop
+    def isBetterGuess(guess: Double): Boolean = {
+      Math.abs(x / guess - guess) / guess < 0.00001
+    }
+
+    def improveGuess(guess: Double): Double = {
+      (guess + x / guess) / 2
+    }
+    squareRootItr(1.0)
+
+  }
+ /* println(squareRoot(2))
+  println(squareRoot(4))
+  println(squareRoot(0.001))
+  println(squareRoot(0.1e-20))
+  println(squareRoot(1.0e20))
+  println(squareRoot(1.0e60))
+  */
+  def tailRecursiveFactorial (x: BigInt) : BigInt = {
+    def loop(acc: BigInt, x: BigInt) : BigInt =
+      if(x==0) acc
+      else loop(acc*x, x-1)
+    loop(1,x)
+  }
+  println(tailRecursiveFactorial(4))
+
 
 }
